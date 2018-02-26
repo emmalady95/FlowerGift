d8f717b63d235364bfaa0ee2d34ea90a0bb14838
emmalady95 <quynt08@gmail.com>
2018-02-26 13:23:01 +0700

    Flower Shop - Design by Emma Nguyen

---
+++ assets/navigation.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liz Nguyen
+ * Date: 25/02/2018
+ * Time: 13:38
+ */
+?>
+
+<div class="js-sticky">
+    <div class="fh5co-main-nav">
+        <div class="container-fluid">
+            <div class="fh5co-menu-1">
+
+                <a href="#" data-nav-section="about">Thông tin&numsp;</a>
+                <a href="#" data-nav-section="features">Nổi bật</a>
+                <a href="#" data-nav-section="menu">Sản phẩm&numsp;</a>
+            </div>
+            <div class="fh5co-logo">
+                <a href="index.php" style = "color: #ff1c69">heart</a>
+            </div>
+            <div class="fh5co-menu-2">
+                <a href="#" data-nav-section="events">Dịch vụ&numsp;</a>
+                <a href="#" data-nav-section="reservation">Liên hệ&numsp;</a>
+                <a href="login.php" data-nav-section="home">Đăng nhập</a>
+            </div>
+        </div>
+
+    </div>
+
+</div>
